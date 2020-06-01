bb0400ac661270c61cd17b0a16a393b166f6ad61
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-05-31 22:37:42 -0700

    finished answering QA

--- smurfs/src/actions/action.js
+++ smurfs/src/actions/action.js
@@ -42,14 +42,3 @@ export const addSmurf = newSmurf => dispatch => {
     });
 };
 
-export const deleteSmurf = id => dispatch =>{
-    dispatch( { type: DELETE_SMURF, id: id })
-
-    axios.delete(`http://localhost:3333/smurfs/${id}`)
-    .then(res=>{
-        dispatch({ type:DELETE_SMURF, id:id })
-    })
-        .catch(err=>{
-        console.log(err)
-    })
-}
